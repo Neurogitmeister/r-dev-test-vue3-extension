40acbd6854fbaaf675b5398c897649e106acaa52
Maxim Zakharov <maximzakharov-z@ya.ru>
2025-02-06 18:06:45 +0700

    Add notifier url logic and fix message

--- src/ui/content/notifier/notifier.vue
+++ src/ui/content/notifier/notifier.vue
@@ -1,12 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const img = chrome.runtime.getURL("src/assets/logo.png")
+
+const url = inject<ComputedRef<string>>("url")
+const { getMerchantByUrl } = useMerchantsStore()
+const merchant = computed(() => (url ? getMerchantByUrl(url.value) : undefined))
+</script>
 
 <template>
-  <div class="notifier">Hello from notifier!</div>
+  <div
+    v-if="!!merchant"
+    class="notifier"
+  >
+    <img :src="img" />
+    {{ `Hello from notifier, ${merchant.domain}!` }}
+  </div>
 </template>
 
 <style scoped lang="scss">
 .notifier {
   position: fixed;
+  z-index: 2147483647 !important;
   top: 60px;
   right: 60px;
   border-radius: 8px;
@@ -16,5 +29,12 @@
   background: white;
   color: black;
   padding: 20px;
+  display: flex;
+  align-items: center;
+  img {
+    height: 16px;
+    width: 16px;
+    margin-right: 8px;
+  }
 }
 </style>
